c9886b793f4147aaa287d0136b9c7611fb6ea4a9
Devan Moylan <dmmoylan@gmail.com>
2017-05-22 19:36:08 +0000

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,14 @@
+
+
+
+module MetaDancing
+
+  def metadancing
+    "This class produces objects that love to dance."
+  end
+
+  def metadata
+    "This class produces objects that love to dance."
+  end
+
+end
